a0811090e119ea91f662abcca4268196074cba99
Simon Karen <simonkaren21@gmail.com>
2024-10-20 19:18:36 +0800

    Create user registration

---
+++ app/Livewire/NewUser.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+
+class NewUser extends Component
+{
+    public $name;
+    public $email;
+    public $password;
+
+    public function submit(){
+
+        $this->validate([
+            'name'=>'required|min:2',
+            'email'=>'required|email|unique:user,email',
+            'password'=>'required|min:8'
+        ]);
+        User::create([
+            'name'=> $this->name,
+            'email'=> $this->email,
+            'password'=> $this->password,
+        ]);
+
+        $this->reset(['name', 'email','password']);
+        session()->flash('message', 'Register successfully');
+    }
+
+    public function render()
+    {
+        return view('livewire.new-user',
+        [
+            'users'=> User::all(),
+        ]);
+    }
+}
--- resources/views/livewire/user.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="flex p-10 items-center flex-col">
-<h1 class="text-3xl mb-2">USER</h1>
-</div>
