40d5e22d99ab309ad887640087f57f907bc9510f
Simon Karen <simonkaren21@gmail.com>
2024-10-20 13:21:07 +0800

    branch activity changes

--- resources/views/livewire/counter-new.blade.php
+++ resources/views/livewire/counter-new.blade.php
@@ -5,7 +5,8 @@
         Decrement</button>
         <button wire:click="increment" type="button" class="text-white bg-purple-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">
         Increment</button>
-
-</div>
+        
+    </div>
+    <h1> Upadeted to activity branch</h1>
 
 </div>
